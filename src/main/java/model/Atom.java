24d14a73ee3c8c8bbfd7baa8096b141b23c4a5be
JohnPooley <thevinylvandal@hotmail.com>
2021-10-20 11:49:45 +0100

    create Atom model with logic for numbers and words

---
+++ src/main/java/model/Atom.java
@@ -0,0 +1,54 @@
+package model;
+
+public class Atom {
+    private String word;
+    private Long number;
+    private AtomType type;
+
+
+    public Atom(String word) {
+        this.word = word;
+        this.number = null;
+        this.type = AtomType.Word;
+    }
+    public Atom( Long number) {
+        this.word = null;
+        this.number = number;
+        this.type = AtomType.Number;
+    }
+
+    public Long getNumber() {
+        if (type == AtomType.Number) {
+        return number;
+        } else {
+            return 0L;
+        }
+    }
+
+    public void setNumber(Long number) {
+        if (this.type != AtomType.Word) {
+            this.type = AtomType.Number;
+            this.number = number;
+        }
+    }
+
+    public String getWord() {
+        if (this.type == AtomType.Word) {
+        return word;
+        } else {
+            return number.toString();
+        }
+    }
+
+    public void setWord(String word) {
+        if (this.type != AtomType.Number) {
+            this.type = AtomType.Word;
+            this.word = word;
+        }
+    }
+
+    public AtomType getType() {
+        return this.type;
+    }
+
+}
---
+++ src/test/java/model/AtomTest.java
@@ -0,0 +1,32 @@
+package model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AtomTest {
+
+    @Test
+    public void canSetAndGetWordType() {
+        Atom word1 = new Atom("word1");
+        Atom word2 = new Atom("word2");
+        word2.setWord("word3");
+        word1.setNumber(23L);
+        assertEquals(AtomType.Word, word1.getType());
+        assertEquals(0L, word1.getNumber());
+        assertEquals("word3", word2.getWord());
+    }
+
+    @Test
+    public void canSetAndGetNumberType() {
+        Atom number1 = new Atom(15L);
+        Atom number2 = new Atom(6L);
+        number2.setWord("word3");
+        number1.setNumber(23L);
+        assertEquals(AtomType.Number, number1.getType());
+        assertEquals(23L, number1.getNumber());
+        assertEquals("6", number2.getWord());
+    }
+
+
+}
